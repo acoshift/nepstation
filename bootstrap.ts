84bf260c5dbdc29f21a914751ff74adb8544d126
Thanatat Tamtan <acoshift@gmail.com>
2015-12-11 01:16:11 +0700

    update

---
+++ bootstrap.ts
@@ -0,0 +1,14 @@
+import {
+  bootstrap,
+  Component
+} from 'angular2/angular2';
+
+@Component({
+  selector: 'app',
+  template: '<h1>Test</h1>'
+})
+class App {
+
+}
+
+bootstrap(App);
--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,11 @@ var gulp = require('gulp'),
 
 var paths = {
   src: './src/',
-  build: './build/',
-  test: './test/',
-  dist: './dist/'
+  build: './build/'
 };
 
-gulp.task('clean-build', function() {
+gulp.task('clean', function() {
   return del([paths.build]);
-});
-
-gulp.task('clean-dist', function() {
-  return del([paths.dist]);
 });
 
 gulp.task('js', function() {
@@ -28,26 +22,6 @@ gulp.task('json', function() {
   return gulp.src(paths.src + '**/*.json')
     .pipe(gulp.dest(paths.build));
 });
-
-gulp.task('dist', ['build'], function() {
-  return gulp.src([paths.build + '**/*', 'package.json', 'README.md'])
-    .pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('test', ['build'], function() {
-  /*return gulp.src('test.js', { cwd: paths.test, read: false })
-    .pipe(mocha());*/
-  return;
-});
-
-gulp.task('watch', function() {
-  gulp.watch(paths.src + '**/*.js', ['js']);
-  gulp.watch(paths.src + '**/*.json', ['json']);
-});
-
-gulp.task('clean', ['clean-dist', 'clean-build']);
-
-gulp.task('test', ['build', 'test-parser', 'test-response']);
 
 gulp.task('build', ['json', 'js']);
 
