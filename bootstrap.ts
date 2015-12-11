e026806d89f1806d926f514faf784d21aa0935e0
Thanatat Tamtan <acoshift@gmail.com>
2015-12-11 15:39:21 +0700

    update

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,14 +1,34 @@
+/// <reference path="node_modules/angular2/angular2.d.ts" />
+
 import {
   bootstrap,
-  Component
+  provide,
+  Component,
+  NgModel,
+  FORM_DIRECTIVES
 } from 'angular2/angular2';
+
+import {
+  ROUTER_PROVIDERS,
+  APP_BASE_HREF,
+
+} from 'angular2/router';
 
 @Component({
   selector: 'app',
-  template: '<h1>Test</h1>'
+  directives: [NgModel, FORM_DIRECTIVES],
+  template: `<h1>Test {{name}}</h1><input type="text" [(ng-model)]="name">`
 })
 class App {
+  public name: string;
 
+  constructor() {
+    this.name = 'my name';
+  }
+
+  change(v) {
+    this.name = v;
+  }
 }
 
 bootstrap(App);
