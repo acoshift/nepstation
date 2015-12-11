bfef93abdf7508bdf2a03285e2fef1704d237b8e
Thanatat Tamtan <acoshift@gmail.com>
2015-12-11 21:20:38 +0700

    update

---
+++ src/app/app.spec.ts
@@ -0,0 +1,53 @@
+import {
+  it,
+  inject,
+  injectAsync,
+  beforeEachProviders,
+  TestComponentBuilder
+} from 'angular2/testing';
+
+import {Component, provide} from 'angular2/angular2';
+import {BaseRequestOptions, Http} from 'angular2/http';
+import {MockBackend} from 'angular2/http/testing';
+
+// Load the implementations that should be tested
+import { App, XLarge } from '../../src/app/app';
+
+// Create a test component to test directives
+@Component({
+  template: '',
+  directives: [XLarge]
+})
+class TestComponent {
+}
+
+describe('x-large directive', () => {
+  it('should sent font-size to x-large', injectAsync([TestComponentBuilder], (tcb) => {
+    return tcb.overrideTemplate(TestComponent, '<div x-large>Content</div>')
+      .createAsync(TestComponent).then((fixture: any) => {
+        fixture.detectChanges();
+        let compiled = fixture.debugElement.nativeElement.children[0];
+        expect(compiled.style.fontSize).toBe('x-large');
+      });
+  }));
+
+});
+
+describe('App', () => {
+  // provide our implementations or mocks to the dependency injector
+  beforeEachProviders(() => [
+    App,
+    BaseRequestOptions,
+    MockBackend,
+    provide(Http, {useFactory:
+      function(backend, defaultOptions) {
+        return new Http(backend, defaultOptions);
+      },
+      deps: [MockBackend, BaseRequestOptions]})
+  ]);
+
+  it('should have a title', inject([App], (app) => {
+    expect(app.title).toEqual('Angular 2');
+  }));
+
+});
