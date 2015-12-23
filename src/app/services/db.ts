80320755afe520f33269241bc79a77d11950a21f
Thanatat Tamtan <acoshift@gmail.com>
2015-12-24 00:41:27 +0700

    DbService supports cache, etag

--- src/app/services/db.ts
+++ src/app/services/db.ts
@@ -9,11 +9,15 @@ import {
   Headers,
 } from 'angular2/http';
 
+import { Observable } from 'rxjs';
+
+import { CacheService } from './cache';
+
 import _ = require('lodash');
 
 @Injectable()
 export class DbService {
-  constructor(private http: Http) { }
+  constructor(private http: Http, private cache: CacheService) { }
 
   server = 'https://farkpage.com/nepdb';
 
@@ -27,12 +31,31 @@ export class DbService {
     let token = sessionStorage.getItem('token') || localStorage.getItem('token');
     if (token) headers.append('Authorization', 'Bearer ' + token);
 
+    let etag = this.cache.etag(body);
+    if (etag) headers.append('If-None-Match', etag);
+
     return this.http.request(new Request({
       method: RequestMethod.Post,
       url: this.server,
       body: body,
       headers: headers
-    })).map(x => x.json());
+    })).catch(err => {
+      return Observable.create(observer => {
+        if (err.status === 304) {
+          observer.next(err);
+          observer.complete();
+        } else {
+          observer.error(err);
+        }
+      });
+    }).map(res => {
+      if (res.status === 304) {
+        return this.cache.data(body);
+      }
+      let r = res.json();
+      if (!r.error) this.cache.cache(body, res.headers.get('etag'), r);
+      return r;
+    });
   }
 
   login(params: any) {
