e723e687ca7d9ddddcf22b5f68a54f4d4731778e
Thanatat Tamtan <acoshift@gmail.com>
2015-12-24 02:22:47 +0700

    format code

--- src/app/services/admin/logs.ts
+++ src/app/services/admin/logs.ts
@@ -1,21 +1,21 @@
 import { Injectable } from 'angular2/core';
-import * as Rx from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { DbService } from '../db';
 import { ReadOnlyModelService } from '../../models/services';
 import { Log } from '../../models/admin';
 
 @Injectable()
 export class LogsService implements ReadOnlyModelService<Log[]> {
-  private logs: Rx.Observable<Log[]>;
-  private log: Rx.Subject<Log[]>;
+  private logs: Observable<Log[]>;
+  private log: Subject<Log[]>;
 
   constructor(private db: DbService) {
-    this.log = new Rx.BehaviorSubject([]);
+    this.log = new BehaviorSubject([]);
     this.logs = this.log.share();
   }
 
   refresh() {
-    this.db.nepq('query', 'db.logs', [{}, {limit: 30}], {
+    this.db.nepq('query', 'db.logs', [{}, {limit: 0}], {
       _id: 1,
       q: { method: 1, name: 1 },
       t: { payload: { sub: 1 } } })
@@ -24,7 +24,7 @@ export class LogsService implements ReadOnlyModelService<Log[]> {
         if (r.error) return this.log.error(r.error);
         this.log.next(r);
       },
-      e => this.log.error(e)
+      e => { ; }//this.log.error(e)
     );
   }
 
