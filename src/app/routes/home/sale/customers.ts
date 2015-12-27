c921f3e42d5292426fbb4613caed1ed757632734
Thanatat Tamtan <acoshift@gmail.com>
2015-12-28 00:50:24 +0700

    update

--- src/app/routes/home/sale/customers.ts
+++ src/app/routes/home/sale/customers.ts
@@ -1,11 +1,10 @@
 import { Component, View } from 'angular2/core';
 import { Subject, Subscriber } from 'rxjs';
 import { NavbarService, CustomersService } from '../../../services';
-import { PaginationComponent, TableComponent } from '../../../components';
+import { PaginationComponent, TableComponent, AlertComponent } from '../../../components';
 import { Log } from '../../../models';
 import _ = require('lodash');
 import moment = require('moment');
-import { TimestampPipe, MomentPipe, ReversePipe, FilterPipe, RepeatPipe, PagePipe, CountPipe } from '../../../pipes';
 import { Directives } from '../../../directives';
 declare var $: any;
 
@@ -15,22 +14,13 @@ declare var $: any;
   styles: [ ],
   directives: [
     PaginationComponent,
+    AlertComponent,
     Directives,
-  ],
-  pipes: [
-    TimestampPipe,
-    MomentPipe,
-    ReversePipe,
-    FilterPipe,
-    RepeatPipe,
-    PagePipe,
-    CountPipe,
   ]
 })
 export class CustomersRoute extends TableComponent {
   constructor(navbar: NavbarService,
-              service: CustomersService,
-              private timestamp: TimestampPipe) {
+              service: CustomersService) {
     super(service);
     navbar.active('sale/customers');
   }
@@ -38,7 +28,7 @@ export class CustomersRoute extends TableComponent {
   get filter() {
     return x => {
       if (!this.search.keyword) return true;
-      switch (this.search.field) {
+      /*switch (this.search.field) {
         case '':
           return x.t.payload.sub.split('/')[0].indexOf(this.search.keyword) !== -1 ||
             x.q.method.indexOf(this.search.keyword) !== -1 ||
@@ -49,7 +39,7 @@ export class CustomersRoute extends TableComponent {
           return x.q.method.indexOf(this.search.keyword) !== -1;
         case 'collection':
           return x.q.name.indexOf(this.search.keyword) !== -1;
-      }
+      }*/
       return false;
     };
   }
