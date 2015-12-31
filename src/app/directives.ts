6ded2d6638a8b3dad9fbe18cfd1cc5e28023a173
Thanatat Tamtan <acoshift@gmail.com>
2016-01-01 02:00:41 +0700

    remove modal directive

--- src/app/directives.ts
+++ src/app/directives.ts
@@ -1,18 +1,15 @@
 import { PopupDirective } from './directives/popup';
 import { DropdownControlValueAccessor } from './directives/dropdown_value_accessor';
-import { ModalDirective } from './directives/modal';
 import { CheckboxDirective } from './directives/checkbox';
 
 export {
   PopupDirective,
   DropdownControlValueAccessor,
-  ModalDirective,
   CheckboxDirective,
 };
 
 export var Directives = [
   PopupDirective,
-  ModalDirective,
   CheckboxDirective,
   DropdownControlValueAccessor,
 ];
