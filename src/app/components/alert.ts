6584268bfb3c5e79075d92c9a53d97b650ed6271
Thanatat Tamtan <acoshift@gmail.com>
2015-12-31 10:14:27 +0700

    remove old code

--- src/app/components/alert.ts
+++ src/app/components/alert.ts
@@ -29,7 +29,7 @@ interface AlertOption {
   },
   template: require('./alert.jade')
 })
-export class AlertComponent extends EventComponent {
+export class AlertComponent {
   title: string = '';
   content: string = '';
   code: string = '';
@@ -38,45 +38,7 @@ export class AlertComponent extends EventComponent {
 
   private _loading: boolean = false;
 
-  constructor(private e: ElementRef) {
-    super();
-  }
-
-  onEvent(event: Event) {
-    if (event.name === 'alert') {
-      this._loading = false;
-      this.title = event.data.title || '';
-      this.content = event.data.content || '';
-      this.code = event.data.code || '';
-      this.buttons = event.data.buttons || [ 'ok' ];
-      this.wait = event.data.wait || false;
-      $(this.e.nativeElement)
-        .modal({
-          closable: event.data.closable || false,
-          allowMultiple: event.data.allowMultiple || true,
-          observeChanges: true,
-          offset: event.data.offset || Number.POSITIVE_INFINITY,
-          transition: event.data.transition,
-          duration: event.data.duration,
-          onShow: event.data.onShow,
-          onVisible: event.data.onVisible,
-          onHide: event.data.onHide,
-          onHidden: event.data.onHidden,
-          onApprove: () => {
-            event.data.onApprove();
-            if (this.wait) {
-              this._loading = true;
-              return false;
-            };
-          },
-          onDeny: event.data.onDeny
-        })
-        .modal('show');
-    } else if (event.name === 'alert.hide') {
-      $(this.e.nativeElement).modal('hide');
-      this._loading = false;
-    }
-  }
+  constructor(private e: ElementRef) {}
 
   show(opt?: AlertOption) {
     if (!opt) opt = {};
