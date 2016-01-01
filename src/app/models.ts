d11e37fb16a57b5712b1fd314d4d0e8deece73fc
Thanatat Tamtan <acoshift@gmail.com>
2016-01-01 14:32:49 +0700

    remove event, event handler

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,6 +1,4 @@
 export * from './models/id';
-export * from './models/event';
-export * from './models/eventhandler';
 export * from './models/admin';
 export * from './models/page';
 export * from './models/common';
