c90eb0f229a436f731fa2643b0beb11c711738c5
Thanatat Tamtan <acoshift@gmail.com>
2016-01-03 15:50:38 +0700

    add product groups

--- src/app/modules/stock/router.ts
+++ src/app/modules/stock/router.ts
@@ -1,5 +1,6 @@
 import { Component } from 'angular2/core';
 import { RouteConfig, RouterOutlet } from 'angular2/router';
+import { ProductGroupsRoute } from './routes/product_groups';
 import { ProductUnitsRoute } from './routes/product_units';
 import { ProductTypesRoute } from './routes/product_types';
 
@@ -8,6 +9,7 @@ import { ProductTypesRoute } from './routes/product_types';
   directives: [ RouterOutlet ]
 })
 @RouteConfig([
+  { path: '/product_groups', name: 'ProductGroups', component: ProductGroupsRoute },
   { path: '/product_units', name: 'ProductUnits', component: ProductUnitsRoute },
   { path: '/product_types', name: 'ProductTypes', component: ProductTypesRoute },
 ])
